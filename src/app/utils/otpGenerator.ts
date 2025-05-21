01566a2763962073f38ded0aaf99bf59258b648d
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2025-05-21 14:49:59 +0600

    updated otp now 6 digits

--- src/app/utils/otpGenerator.ts
+++ src/app/utils/otpGenerator.ts
@@ -1,5 +1,5 @@
 export const generateOtp = () => {
-  const otp = Math.floor(1000 + Math.random() * 9000); // Generate a random 6-digit number
+  const otp = Math.floor(100000 + Math.random() * 900000); // Generate a random 6-digit number
   
   return otp.toString(); // Convert to string and return
 };
