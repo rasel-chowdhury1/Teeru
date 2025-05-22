6f0368f3af597c177d7d2cc9508b89d2f5d83d0e
Rasel Chowdhury <rasel.chowdhury0001@gmail.com>
2025-05-22 10:38:08 +0800

    updated

--- src/app/modules/otp/otp.validation.ts
+++ src/app/modules/otp/otp.validation.ts
@@ -4,7 +4,7 @@ const verifyOtpZodSchema = z.object({
   body: z.object({
     otp: z
       .string({ required_error: 'otp is required' })
-      .length(4, { message: 'otp must be exactly 6 characters long' }),
+      .length(6, { message: 'otp must be exactly 6 characters long' }),
   }),
 });
 
